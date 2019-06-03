5cc1452cf143d4a27c505e07515f4d22f32a4077
donatelek <47256040+donatelek@users.noreply.github.com>
2019-06-03 23:57:32 +0200

    fixing layout bcg

--- src/Components/App.js
+++ src/Components/App.js
@@ -69,6 +69,14 @@ class App extends Component {
         })
     }
   }
+  componentDidMount(){
+    setTimeout(function () {
+      let viewheight = $(window).height();
+      let viewwidth = $(window).width();
+      let viewport = document.querySelector("meta[name=viewport]");
+      viewport.setAttribute("content", "height=" + viewheight + "px, width=" + viewwidth + "px, initial-scale=1.0");
+  }, 300);
+  }
   changingKej=(kej)=>{
     this.setState({
       kej
